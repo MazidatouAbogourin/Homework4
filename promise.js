ebe3e0b4903db7cafb26274c130a47e8f24dffdd
Mazidatou Abogourin <mazidatou@gmail.com>
2025-04-23 13:16:21 -0500

    the file was changed

--- promise.js
+++ promise.js
@@ -5,12 +5,11 @@ const pokemonPromises = pokename.map((name) =>
     return res.json();
   })
 );
-console.log("Pomise.Any");
+
 Promise.any(pokemonPromises).then((data) => {
-  console.log("promise " + data.name);
+  console.log("Inside promise.any " + data.name);
 });
 
-console.log("Pomise.Race");
 Promise.race([...pokemonPromises, Promise.reject(555)]).then((data) => {
   console.log("inside race " + data.name);
 });
@@ -21,20 +20,15 @@ async function getData(url) {
   return input;
 }
 
-let promise1 = getData("https://pokeapi.co/api/v2/pokemon-species/aegislash");
-let promise2 = getData("https://pokeapi.co/api/v2/ability/battle-armor");
-let promise3 = getData("https://pokeapi.co/api/v2/pokemon/ditto");
-
-console.log("Pomise.All");
 //Promise All
-Promise.all([promise1, promise2, promise3]).then(function (data) {
+Promise.all(pokemonPromises).then(function (data) {
   data.forEach((c, i) => console.log(i + " " + c.name));
 });
-Promise.all([promise1, promise2, promise3, Promise.reject(555)]).catch((e) =>
+Promise.all([...pokemonPromises, Promise.reject(555)]).catch((e) =>
   console.log(e)
 );
 
 //Promise All Settled
-Promise.allSettled([promise1, promise2, promise3, Promise.reject(555)]).then(
-  (d) => console.log(d)
+Promise.allSettled([...pokemonPromises, Promise.reject(555)]).then((d) =>
+  console.log(d)
 );
